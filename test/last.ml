3b25f21ec175b6b7e8b440d50e786f5a0129ae41
Rosalie Aguillon <rosval6578@gmail.com>
2023-06-29 23:06:12 +0200

    add tests for last

--- test/last.ml
+++ test/last.ml
@@ -1,4 +1,16 @@
 open Exolib.Last
 
-let test_none () =
-  Alcotest.(check option) "same type" None (last None);
+let test_type_int () =
+  Alcotest.(check (option int)) "same type" None (last []);
+  Alcotest.(check (option int)) "same type" (Some 3) (last [3]);
+  Alcotest.(check (option int)) "same type" (Some 3) (last [1;2;3])
+
+let test_type_string () =
+  Alcotest.(check (option string)) "same type" None (last []);
+  Alcotest.(check (option string)) "same type" (Some "c") (last ["c"]);
+  Alcotest.(check (option string)) "same type" (Some "c") (last ["a";"b";"c"])
+
+let test_type_char () =
+  Alcotest.(check (option char)) "same type" None (last []);
+  Alcotest.(check (option char)) "same type" (Some 'c') (last ['c']);
+  Alcotest.(check (option char)) "same type" (Some 'c') (last ['a';'b';'c'])
