8b44e57280a45884c9ef8905f83f8eafabf77fda
Rosalie Aguillon <rosval6578@gmail.com>
2023-06-29 23:15:21 +0200

    add notes

--- lib/last.ml
+++ lib/last.ml
@@ -1,5 +1,5 @@
+(* Return the last element of a list, or None if the list is empty *)
 let rec last = function 
-  | [] -> None
-  | [ x ] -> Some x
-  | _ :: t -> last t;;
-(* val last : 'a list -> 'a option = <fun> *)
+  | [] -> None (* Base case: empty list *)
+  | [x] -> Some x (* Case where there is only one element in the list *)
+  | _ :: t -> last t (* Recursive call with the tail of the list *)
